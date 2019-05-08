6fed327f27ed27b3ae3c070ece247aa9da51718d
adaltezze <adaltezze@gmail.com>
2019-05-08 23:57:14 +0500

    update random-planet, add error-indicator

---
+++ src/components/error-indicator/error-indicator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorIndicator = () => {
+    return (
+        <div className="alert alert-danger" role="alert">
+            Something went wrong
+        </div>
+    );
+}
+
+export default ErrorIndicator;
--- src/components/random-planet/random-planet.jsx
+++ src/components/random-planet/random-planet.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { getPlanet } from '../../services/swapi-service.js';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 import './random-planet.scss';
 
@@ -10,10 +11,12 @@ const PlanetView = (props) => {
     return (
         <Fragment>
             <div className="col-4 p-0">
-                <img
-                    src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-                    className="card-img-top"
-                    alt={name} />
+                <div
+                    style={{
+                        background: `url(https://starwars-visualguide.com/assets/img/planets/${id}.jpg) no-repeat center / cover,
+                                     url(https://starwars-visualguide.com/assets/img/placeholder.jpg) no-repeat center top / cover`
+                    }}
+                    className="card-img-top" />
             </div>
             <div className="card-body col-8">
                 <h3 className="card-title h5">
@@ -38,7 +41,8 @@ const PlanetView = (props) => {
 export default class RandomPlanet extends Component {
     state = {
         planet: {},
-        loading: true
+        loading: true,
+        error: false
     };
 
     constructor(props) {
@@ -54,27 +58,43 @@ export default class RandomPlanet extends Component {
         this.setState({ planet, loading: false });
     };
 
+    onPlanetError = (error) => {
+        this.setState({
+            error: true,
+            loading: false
+        });
+    };
+
     updatePlanet() {
         const id = this.randomId;
 
         getPlanet(id)
-            .then(this.onPlanetLoaded);
+            .then(this.onPlanetLoaded)
+            .catch(this.onPlanetError);
     }
 
     render() {
-        const { loading, planet } = this.state;
+        const { loading, error, planet } = this.state;
+
+        const errorMessage = error ? (
+            <div className="d-flex justify-content-center align-items-center w-100">
+                <ErrorIndicator />
+            </div>
+        ) : null;
+        const speinner = loading? (
+            <div className="d-flex justify-content-center align-items-center w-100">
+                <Spinner />
+            </div>
+        ) : null;
+        const content = !(loading || error) ? <PlanetView planet={planet} /> : null;
+
         return (
             <section className="random-planet py-4">
                 <div className="container">
-                    <div className="card flex-row border ">
-                        {loading
-                            ? (
-                                <div className="d-flex justify-content-center align-items-center w-100">
-                                    <Spinner />
-                                </div>
-                            )
-                            : <PlanetView planet={planet} />
-                        }
+                    <div className="card flex-row">
+                        {errorMessage}
+                        {speinner}
+                        {content}
                     </div>
                 </div>
             </section>
